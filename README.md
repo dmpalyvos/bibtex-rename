4e8c0d4496d7e94f6c2da6fefbd7db293b5db955
Dimitris Palyvos Giannas <palyvos@chalmers.se>
2019-08-06 12:34:29 +0200

    Add initial files

---
+++ README.md
@@ -0,0 +1,26 @@
+# Rename BibTex References
+
+## Introduction
+
+This is is a simple tool that can automatically update your LaTeX code if you want to change the key format of your BibTex references.
+
+For example, you have a document where the reference keys have the format `author_title_year` and you want to update the reference key format to `authorTitleYearJournalName`. Depending on the size of the document(s), changing all the references by hand can be quite tedious.
+
+## Requirements
+
+- python 3.6
+- bibtexparser (`pip install bibtexparser`)
+
+## Usage
+
+The tool, `transform.py` takes as input the original tex file, the original bibliography and the updated bibliography (with the new key format) and outputs a new tex file where the reference keys have been updated.
+
+```bash
+transform.py [-h] --tex TEX --oldbib OLDBIB --newbib NEWBIB [--out OUT]
+```
+
+## Tips & Tricks
+
+- The tool uses the content of the bibliography entries (e.g., title, year) to determine the correct key association. Thus, to maximize your chances of success, try to not change the content of the the entries too much from your old to your new bibliography. You can always make any changes you want after updating they keys.
+- The updated bibliography can be a superset of the original one. The tool should have no problem, as long as there are no entries that are very similar (e.g., same title and year).
+- While I have tweaked the tool and it works good enough for my needs, feel free to submit a pull request if you run into any problems. :)
---
+++ transform.py
@@ -0,0 +1,83 @@
+import bibtexparser
+import re
+import argparse
+import sys
+
+
+# Remove month entries form raw BibTex 
+# because bibtexparser complains about month format from Zotero
+def removeMonths(bibtex):
+   lines = []
+   for line in bibtex:
+      if 'month' not in line:
+         lines.append(line)
+   return ''.join(lines)
+
+
+def simplifiedString(input):
+   return re.sub(r'\W+', '', input).lower()
+
+
+def saveLookup(bibKey, bibEntry, lookup):
+   year = bibEntry['year'] if 'year' in bibEntry else ''
+   contentKey = simplifiedString(bibEntry['title']) + year
+   if contentKey in lookup:
+      raise Exception(f'Duplicate content key "{contentKey}"!')
+   lookup[contentKey] = bibKey
+
+
+if __name__ == '__main__':
+
+   parser = argparse.ArgumentParser()
+   parser.add_argument('--tex', help='LaTeX input file', 
+      type=argparse.FileType('r'), required=True)
+   parser.add_argument('--oldbib', help='Old bibliography file', 
+      type=argparse.FileType('r'), required=True)
+   parser.add_argument('--newbib', help='New bibliography file', 
+      type=argparse.FileType('r'), required=True)
+   parser.add_argument('--out', help='Output LaTeX file', 
+      type=argparse.FileType('w'), default='out.tex')
+
+   args = parser.parse_args()
+
+   oldBibliography = bibtexparser.loads(removeMonths(args.oldbib))
+   newBibliography = bibtexparser.loads(removeMonths(args.newbib))
+   args.oldbib.close()
+   args.newbib.close()
+
+   oldLookup = dict()
+   newLookup = dict()
+
+   for key, entry in oldBibliography.entries_dict.items():
+      saveLookup(key, entry, oldLookup)
+
+   for key, entry in newBibliography.entries_dict.items():
+      saveLookup(key, entry, newLookup)
+
+   # Replace BibTeX keys based on content keys
+   text = args.tex.read()
+   errorCount = 0
+   for contentKey, oldBibTexKey in oldLookup.items():
+      # Find new key through content comparison
+      if contentKey not in newLookup:
+         print(f'[!] Error replacing {oldBibTexKey}: Relevant entry not found in {args.newbib.name} (was it a duplicate?)', file=sys.stderr)
+         errorCount += 1
+         continue
+      newBibTexKey = newLookup[contentKey]
+      try:
+         # Find citation(s) for the current key and replace it only inside these sections
+         text, replacements = re.subn(r'(\\cite{[^\}]*)(' + oldBibTexKey + r')([^\}]*})', r'\1' + newBibTexKey + r'\3', text)
+         print(f'[>] Replaced {oldBibTexKey} -> {newBibTexKey} [{replacements} replacements]')
+      except Exception as e:
+         print(f'[!] Error replacing {oldBibTexKey}: {e}', file=sys.stderr)
+         errorCount += 1
+   args.tex.close()
+
+   try:
+      # Write new TeX file   
+      args.out.write(text)
+      print(f'---- Finished output to {args.out.name} with {errorCount} errors ----')
+   except Exception as e:
+      raise e
+   finally:
+      args.out.close()
